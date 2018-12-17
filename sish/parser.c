531bd5374e5ba755b947436771e9fe9edb3da9b9
Yuxi Wang <wyxr18@sina.com>
2018-12-17 04:36:40 -0500

    parser prototype

--- sish/parser.c
+++ sish/parser.c
@@ -1,1 +1,268 @@
 #include "parser.h"
+
+bool 
+initialize_parser(struct parser *commandparser, struct commandlist *commands)
+{
+	if (commandparser == NULL || commands == NULL)
+		return false;
+	(void)memset(commandparser, 0, sizeof(struct parser));
+	commandparser->commands = commands;
+	return true;
+}
+
+int
+parse_command(struct parser *commandparser, const char *commandtext, struct commandlist *commands)
+{
+	if (commandparser == NULL || commandtext == NULL || commands == NULL)
+		return -1;
+	if (!initialize_parser(commandparser, commands))
+		return -1;
+	if (!lexical_analysis(commandparser, commandtext))
+		return -2;
+	if (!syntactic_analysis(commandparser))
+		return -3;
+	return 0;
+	
+}
+
+bool
+lexical_analysis(struct parser *commandparser, const char *commandtext)
+{
+	size_t textlen;
+	commandparser->state = PLAINSTATE;
+	textlen = strlen(commandtext);
+	for (size_t n = 0; n != textlen; ++n) {
+		if (is_plaintext(commandtext[n])) {
+			if (!flushopbuffer(commandparser))
+				return false;
+			if (!write_currenttoken(commandparser, commandtext[n]))
+				return false;
+			commandparser->state = PLAINSTATE;
+			continue;
+		}
+		if (is_controlop(commandtext[n])) {
+			if (!flushopbuffer(commandparser))
+				return false;
+			if (!set_currentctrop(commandparser, commandtext[n]))
+				return false;
+			commandparser->state = CTROPSTATE;
+			continue;
+		}
+		if (is_singlecommonop(commandtext[n])) {
+			if (!flushopbuffer(commandparser))
+				return false;
+			if (!set_currentctrop(commandparser, commandtext[n]))
+				return false;
+			commandparser->state = CTROPSTATE;
+			continue;
+		}
+		if (is_doublecommonop(commandtext[n])) {
+			if (commandparser->state == D_COMOPSTATE) {
+				commandparser->opbuffer[1] = commandtext[n];
+				++commandparser->opbufferlen;
+				if (!flushopbuffer(commandparser))
+					return false;
+			}
+			else {
+				commandparser->opbuffer[0] = commandtext[n];
+				++commandparser->opbufferlen;				
+			}
+			commandparser->state = D_COMOPSTATE;
+			continue;
+		}
+		if (is_seperator(commandtext[n])) {
+			if (commandparser->state != SPSTATE) {
+				if (!flushopbuffer(commandparser))
+					return false;
+				if (!set_nexttoken(commandparser))
+					return false;
+			}
+			commandparser->state = SPSTATE;
+			continue;
+		}
+		return false;
+	}
+	return true;
+}
+
+bool
+syntactic_analysis(struct parser *commandparser)
+{
+	size_t commandnum, tokennum;
+	struct token *currenttoken, *pretoken, *nexttoken;
+	commandnum = commandparser->commands->commandnum;
+	for (int i = 0; i != commandnum; ++i) {
+		tokennum = commandparser->commands->comlist[i].tokennum;
+		for (int n = 0; n != tokennum; ++n) {
+			currenttoken = &commandparser->commands->comlist[i].tokens[n];
+			if (currenttoken->comop != NOCTROP) {
+				if (n == tokennum - 1)
+					return false;
+				if (n != 0) {
+					pretoken = currenttoken - 1;
+					if (is_digits(pretoken->content))
+						return false;
+					nexttoken = currenttoken + 1;
+					if (nexttoken->comop == NOCTROP)
+						return false;
+				}
+			}
+		}
+	}
+	return true;
+}
+
+bool
+is_plaintext(char c)
+{
+	//cant be ' or " or `
+	if(strpbrk(&c, "`\'\"") != NULL)
+		return false;
+	if (is_controlop(c) || 
+		is_singlecommonop(c) || 
+		is_singlecommonop(c) || 
+		is_seperator(c))
+		return false;
+	return true;
+}
+
+bool 
+is_controlop(char c)
+{
+	if (strpbrk(&c, "&|\n") == NULL)
+		return false;
+	return true;
+}
+
+bool 
+is_singlecommonop(char c)
+{
+	if (strpbrk(&c, "<") == NULL)
+		return false;
+	return true;
+}
+
+bool 
+is_doublecommonop(char c)
+{
+	if (strpbrk(&c, ">") == NULL)
+		return false;
+	return true;
+}
+
+bool 
+is_seperator(char c)
+{
+	if (strpbrk(&c, " \t") == NULL)
+		return false;
+	return true;
+}
+
+bool
+is_digits(const char *text)
+{	
+	if (strspn(text, "0123456789") != strlen(text))
+		return false;
+	return true;
+}
+
+ctrop_type
+get_ctroptype(char c)
+{
+	switch (c) {
+	case '&':
+		return AMPER;
+	case '|':
+		return PIPE;
+	case'\n':
+		return NEWLINE;
+	case ';':
+		return SEMICOLON;
+	default:
+		return NOCTROP;
+	}
+}
+
+commonop_type
+get_singlecommontype(char c)
+{
+	switch (c) {
+	case '<':
+		return RDFROMFILE;
+	default:
+		return NOCOMOP;
+	}
+}
+
+commonop_type
+get_doublecommontype(const char *buffer)
+{
+	if (strcmp(buffer, ">") == 0)
+		return RDTOFILE;
+	if (strcmp(buffer, ">>") == 0)
+		return APTOFILE;
+	return NOCOMOP;
+}
+
+bool
+set_currentctrop(struct parser *commandparser, char c)
+{
+	commandparser->currentcommand->ctrop = get_ctroptype(c);
+	if (!set_nextcommand(commandparser))
+		return false;
+	return true;
+}
+
+bool
+set_nexttoken(struct parser *commandparser)
+{
+	++commandparser->tokenindex;
+	if (commandparser->tokenindex > MAX_TOKEN_NUM)
+		return false;
+	commandparser->characterindex = 0;
+	commandparser->currenttoken = &commandparser->currentcommand->tokens[commandparser->tokenindex];
+	commandparser->currentcharacter = &commandparser->currenttoken->content[0];
+	++commandparser->currentcommand->tokennum;
+	return true;
+}
+
+bool 
+set_nextcommand(struct parser *commandparser)
+{
+	++commandparser->commandindex;
+	if (commandparser->commandindex > MAX_COMMAND_SIZE)
+		return false;
+	commandparser->tokenindex = 0;
+	commandparser->characterindex = 0;
+	commandparser->currentcommand = &commandparser->commands->comlist[commandparser->commandindex];
+	commandparser->currenttoken = &commandparser->currentcommand->tokens[0];
+	commandparser->currentcharacter = &commandparser->currenttoken->content[0];
+	++commandparser->commands->commandnum;
+	return true;
+}
+
+bool 
+flushopbuffer(struct parser *commandparser)
+{
+	commonop_type type;
+	if (commandparser->state == S_COMOPSTATE)	
+		type = get_singlecommontype(commandparser->opbuffer[0]);
+	if (commandparser->state == D_COMOPSTATE)
+		type = get_doublecommontype(commandparser->opbuffer);
+	commandparser->currenttoken->comop = type;
+	commandparser->opbufferlen = 0;
+	if (!set_nexttoken(commandparser))
+		return false;
+	return true;
+}
+
+bool 
+write_currenttoken(struct parser *commandparser, char c)
+{
+	if (commandparser->characterindex >= MAX_COMMANDTEXT_SIZE)
+		return false;
+	commandparser->currenttoken->content[commandparser->characterindex] = c;
+	++commandparser->currenttoken->contentlenth;
+	++commandparser->characterindex;
+	return true;
+}
--- sish/parser.h
+++ sish/parser.h
@@ -2,11 +2,82 @@
 #define _PARSER_
 
 #include <stdbool.h>
+#include "config.h"
 
-struct parser {
+#define MAX_OP_LEN 2
+
+typedef enum parsestate { 
+	PLAINSTATE = 1,
+	CTROPSTATE,
+	S_COMOPSTATE,
+	D_COMOPSTATE,
+	SPSTATE
 	
+}parsestate;
+
+typedef enum ctrop_type { 
+	NOCTROP = 1,
+	NEWLINE,
+	PIPE,
+	AMPER,
+	SEMICOLON
+}ctrop_type;
+
+typedef enum commonop_type { 
+	NOCOMOP = 1,
+	RDTOFILE,
+	APTOFILE,
+	RDFROMFILE
+}commonop_type;
+
+struct token {
+	char content[MAX_COMMANDTEXT_SIZE];
+	size_t contentlenth;
+	commonop_type comop;
 };
 
-bool parse_command(struct parser *, char *, char *);
+struct command {
+	struct token tokens[MAX_TOKEN_NUM];
+	size_t tokennum;
+	ctrop_type ctrop;
+};
+
+struct commandlist {
+	struct command comlist[MAX_COMMAND_SIZE];
+	size_t commandnum;
+};
+
+struct parser {
+	struct commandlist *commands;
+	size_t commandindex;
+	size_t tokenindex;
+	size_t characterindex;
+	struct command *currentcommand;
+	struct token *currenttoken;
+	char *currentcharacter;
+	char opbuffer[2];
+	size_t opbufferlen;
+	parsestate state;
+};
+
+bool initialize_parser(struct parser *, struct commandlist *);
+int parse_command(struct parser *, const char *, struct commandlist *);
+bool lexical_analysis();
+bool syntactic_analysis();
+bool is_commonoptoken(struct token *);
+bool is_plaintext(char);
+bool is_controlop(char);
+bool is_singlecommonop(char);
+bool is_doublecommonop(char);
+bool is_seperator(char);
+bool is_digits(const char *);
+ctrop_type get_ctroptype(char);
+commonop_type get_singlecommontype(char);
+commonop_type get_doublecommontype(const char *);
+bool write_currenttoken(struct parser *, char);
+bool set_currentctrop(struct parser *, char);
+bool set_nexttoken(struct parser *);
+bool set_nextcommand(struct parser *);
+bool flushopbuffer(struct parser *);
 
 #endif // !_PARSER_
